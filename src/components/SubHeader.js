5a078df3192e4a32a7ceb9594931b5cb4f215ecd
Seke Kalq <andrew@indypixels.co>
2024-12-20 21:26:29 -0500

    added subheader

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,13 +1,21 @@
 import styles from './SubHeader.module.css';
+import { MdOutlineFileUpload } from "react-icons/md";
+const iconStyle = { color: "rgb(119,207,190)", fontSize: "2.5em" }
 
 export default function SubHeader () {
     return (
         <div className={styles.subHeader}>
             <div className={styles.subHeaderTitleContainer}>
                 <div className={styles.subHeaderTitle}>
-
+                    how do you look today?
                 </div>
-                <div className={styles.subHeader}></div>
+                <div className={styles.subHeaderSubTitle}>
+                    provide a selfie to learn how AI can improve your fit, drop an image or click here to upload
+                </div>
+            </div>
+            <div className={styles.filler}></div>
+            <div className={styles.uploadSection}>
+                <MdOutlineFileUpload style={iconStyle}/>
             </div>
         </div>
     );
